16b9f2bc85253e06661cf23e281c367b4fa4262a
Fabio Elías Rengifo García <81293357+RengifoGarciaFE@users.noreply.github.com>
2025-12-10 11:49:14 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  const classes = ["diamond", "heart", "spade", "club"];
+  const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomValueIndex = Math.floor(Math.random() * values.length);
+  const card = document.getElementById("card");
+
+  card.className = "card";
+  card.classList.add(classes[randomSuitIndex]);
+
+  document.querySelector(".top-suit").innerHTML = suits[randomSuitIndex];
+  document.querySelector(".value").innerHTML = values[randomValueIndex];
+  document.querySelector(".bottom-suit").innerHTML = suits[randomSuitIndex];
 };
